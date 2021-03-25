39aebfea51e92e62ba374a7ab33c9f4c3d7de9ae
obiwycliffe <obiawycliffe@outlook.com>
2021-03-25 10:47:20 +0300

    Activity Update

--- app/src/main/java/com/example/timefighter/obiwycliffe/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/obiwycliffe/MainActivity.kt
@@ -2,10 +2,70 @@ package com.example.timefighter.obiwycliffe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal var score = 0
+    // internal var score: Int = 0
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal var initialCountDown: Long = 60000
+    internal var countDownInterval: Long = 1000
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // goes after setContentView() to allow layout load first
+        tapMeButton = findViewById(R.id.tapMeButton)
+        gameScoreTextView = findViewById(R.id.gameScoreTextView)
+        timeLeftTextView = findViewById(R.id.timeLeftTextView)
+
+        // ...
+        resetGame()
+
+        // set onClick event listener
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+    }
+
+    private fun incrementScore() {
+        score +=1
+        val newScore = getString(R.string.yourScore, score)
+        gameScoreTextView.text = newScore
+    }
+
+    private fun resetGame() {
+        score = 0
+
+        // set default value for view placeholder
+        gameScoreTextView.text = getString(R.string.yourScore, score)
+
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        gameStarted = false
     }
 }
